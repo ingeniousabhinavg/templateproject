0ff47467f724494470ba1d6f0c1cf4c04eace25e
ingenious <ingeniousabhinav@gmail.com>
2022-09-16 22:01:33 +0530

    Added project model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
+from typing import Tuple
 from django.db import models
 
 # Create your models here.
+
+class Myprojects(models.Model):
+    title = models.CharField(max_length=50)
+    about = models.TextField()
+    url = models.URLField(max_length=200, null=True)
+    banner = models.ImageField(upload_to='banner', null = False)
+    date = models.DateField(auto_now=False, auto_now_add=False, null=False)
+
+    def __str__(self) -> str:
+        return self.title
--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import *
+
+# call your data from models
+projectsData = Myprojects.objects.all()
+
+context = {
+    'projects': projectsData
+}
 
 # Create your views here.
-
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
